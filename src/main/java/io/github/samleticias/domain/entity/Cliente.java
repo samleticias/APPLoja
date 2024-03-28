6ebd921b1dea9bd4388a94f660ecc0b21b8f9d52
samleticias <sammyaleticia09@gmail.com>
2024-03-28 00:50:42 -0300

    validando entidade cliente com cpf e nome obrigatorios

--- src/main/java/io/github/samleticias/domain/entity/Cliente.java
+++ src/main/java/io/github/samleticias/domain/entity/Cliente.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
 
 import java.util.Set;
 
@@ -28,6 +29,8 @@ public class Cliente {
     private String nome;
 
     @Column(name = "cpf", length = 11)
+    @NotEmpty(message = "Campo CPF é obrigatório.")
+    @CPF(message = "Informe um CPF válido.") // annotation pronta para validar CPF
     private String cpf;
 
     @JsonIgnore
