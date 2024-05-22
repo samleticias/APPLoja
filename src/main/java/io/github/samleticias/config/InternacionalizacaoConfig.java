aef386d06bd2513e12ffd27c475d50f217b50d5f
samleticias <sammyaleticia09@gmail.com>
2024-05-22 00:43:55 -0300

    interpolação para validar campos obrigatórios

--- src/main/java/io/github/samleticias/config/InternacionalizacaoConfig.java
+++ src/main/java/io/github/samleticias/config/InternacionalizacaoConfig.java
@@ -1,5 +1,30 @@
 package io.github.samleticias.config;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import java.util.Locale;
+
 // Interpolação: leitura do arquivo com chaves de mensagens personalizadas para validações
+@Configuration
 public class InternacionalizacaoConfig {
+    @Bean
+    public MessageSource messageSource(){
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages"); // messages.properties
+        messageSource.setDefaultEncoding("ISO-8859-1"); // reconhecer codificação de texto
+        messageSource.setDefaultLocale(Locale.getDefault()); // local padrão de onde está rodando a aplicação
+        return messageSource;
+    }
+
+    // interpolação para validar campos obrigatórios
+    @Bean
+    public LocalValidatorFactoryBean validatorFactoryBean(){
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
 }
