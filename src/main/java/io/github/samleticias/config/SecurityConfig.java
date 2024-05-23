38dc6175cc190b18f5eba72e1ea6e0729a56809f
samleticias <sammyaleticia09@gmail.com>
2024-05-23 12:03:03 -0300

    n acredito q funcionou

--- src/main/java/io/github/samleticias/config/SecurityConfig.java
+++ src/main/java/io/github/samleticias/config/SecurityConfig.java
@@ -2,11 +2,21 @@ package io.github.samleticias.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import static org.springframework.security.config.Customizer.withDefaults;
+
 
 // adicionar dependencias de security no pom.xml
-// @Configuration
+@EnableWebSecurity
+@Configuration
 public class SecurityConfig {
-    /*
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -16,11 +26,18 @@ public class SecurityConfig {
                 .httpBasic(withDefaults());
         return http.build();
     }
-
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
-     */
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user")
+                .password(passwordEncoder().encode("1234"))
+                .roles("USER").build());
+        return manager;
+    }
+
 }
